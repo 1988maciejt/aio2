d00d1147b03583def23066a276e120093fac97ad
Maciej Trawka <maciejtr@gmail.com>
2022-09-24 23:12:12 +0200

    Preprocessor and CppProgram class tested

--- bin/libs/cpp_program.py
+++ bin/libs/cpp_program.py
@@ -1,23 +1,30 @@
 from libs.aio import *
-from libs.files import writeFile
+from random import uniform
+import os
+from libs.preprocessor import *
 
 class CppProgram:
   CppFileName = ""
   ExeFileName = ""
+  PreprocessedSourceFileName = ""
   _comp_error = ""
+  _args = ()
+  _kwargs = {}
   Compiled = False
-  def __init__(self, CppFileName : str, ExeFileName = "", Code = "") -> None:
-    self.CppFileName = CppFileName
-    if len(Code) > 1:
-      writeFile(CppFileName, Code)
-    if len(ExeFileName) < 1:
-      self.ExeFileName = CppFileName + ".exe"
+  def __init__(self, CppFileName : str, *args, **kwargs) -> None:
+    self._args = args
+    self._kwargs = kwargs
+    rand = str(int(uniform(100000000, 999999999)))
+    self.CppFileName = os.path.abspath(CppFileName)
+    self.ExeFileName = os.path.abspath(os.path.basename(CppFileName) + f'.{rand}.exe')
+    self.PreprocessedSourceFileName = os.path.abspath(os.path.basename(CppFileName) + f'.{rand}.preprocessed.cpp')
   def __str__(self) -> str:
     return f'{self.ExeFileName}'
   def __repr__(self) -> str:
     return f'CppProgram({self.CppFileName}, {self.ExeFileName})'
   def compile(self) -> bool:
-    Cmd = f'g++ {self.CppFileName} -o {self.ExeFileName}'
+    preprocessFile(self.CppFileName, self.PreprocessedSourceFileName, *self._args, **self._kwargs)
+    Cmd = f'g++ {self.PreprocessedSourceFileName} -o {self.ExeFileName}'
     Result = Aio.shellExecute(Cmd)
     if len(Result) > 2:
       self._comp_error = Result
@@ -28,15 +35,16 @@ class CppProgram:
     return self.Compiled
   def isCompiled(self) -> bool:
     return self.Compiled
-  def run(self, Args = None) -> str:
+  def run(self, *args) -> str:
     if not self.Compiled:
       self.compile()
     if not self.Compiled:
       return None
     Cmd = self.ExeFileName
-    if Aio.isType(Args, []):
-      for Arg in Args:
-        Cmd += " " + str(Arg)
-    elif not Aio.isType(Args, None):
-      Cmd += str(Args)
+    for Arg in args:
+      Cmd += " " + str(Arg)
     return Aio.shellExecute(Cmd)
+  def redefinePreprocessingArgs(self, *args, **kwargs):
+    self._args = args
+    self._kwargs = kwargs
+    self.Compiled = False
--- bin/libs/preprocessor.py
+++ bin/libs/preprocessor.py
@@ -1,14 +1,16 @@
 from libs.files import *
 import re
 
-def preprocessTextToPythonCode(Text : str, *args) -> str:
+def preprocessTextToPythonCode(Text : str, *args, **kwargs) -> str:
   Result = 'from aio import *\n'
+  Result += f'args = {args}\n'
+  Result += f'kwargs = {kwargs}\n'
   Result += 'global PreprocessingResult___\n'
   Result += 'PreprocessingResult___ = ""\n'
   Lines = Text.split('\n')
   Spaces = ""
   for Line in Lines:
-    if len(Line) == 0:
+    if re.match(r'^`\s*end\s*\S*$', Line):
       if len(Spaces) > 0:
         Spaces = Spaces[0:-1]
       continue
@@ -22,23 +24,26 @@ def preprocessTextToPythonCode(Text : str, *args) -> str:
         reFound = re.search(r'([^\`]*)\`\(([^\`]*)\`\)(.*)', Line)
         if reFound:
           if len(reFound[1]) > 0:
-            Result += f'{Spaces}PreprocessingResult___ += "{reFound[1]}"\n'
-          Result += Spaces + "PreprocessingResult___ += str(" + reFound[2] + ")\n"
+            L2 = str(reFound[1]).replace('"', '\\"')
+            Result += f'{Spaces}PreprocessingResult___ += "{L2}"\n'
+          L2 = str(reFound[2]).replace('"', '\\"')
+          Result += f'{Spaces}PreprocessingResult___ += str({L2})\n'
           Line = reFound[3]
           CheckIfLineIsEmpty = True
         else:
           break
       if (not CheckIfLineIsEmpty) or (CheckIfLineIsEmpty and len(Line) > 0):
-        Result += f'{Spaces}PreprocessingResult___ += "{Line}"\n'
+        L2 = Line.replace('"', '\\"')
+        Result += f'{Spaces}PreprocessingResult___ += """{L2}"""\n'
       Result += f'{Spaces}PreprocessingResult___ += "\\n"\n'
   return Result
 
-def preprocessString(Text : str, *args) -> str:
+def preprocessString(Text : str, *args, **kwargs) -> str:
   global PreprocessingResult___
-  exec(preprocessTextToPythonCode(Text, *args))
+  exec(preprocessTextToPythonCode(Text, *args, **kwargs))
   return PreprocessingResult___[0:-1]
 
-def preprocessFile(InputFileName : str, OutputFileName : str, *args):
+def preprocessFile(InputFileName : str, OutputFileName : str, *args, **kwargs):
   Text = readFile(InputFileName)
-  Text = preprocessString(Text, args)
+  Text = preprocessString(Text, *args, **kwargs)
   writeFile(OutputFileName, Text)
