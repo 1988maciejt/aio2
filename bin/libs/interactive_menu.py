c313de251c15b0eb415c4bbab082101948903ce7
Maciej <maciej.trawka@icloud.com>
2022-11-13 18:48:00 +0100

    menu improvements

--- bin/libs/interactive_menu.py
+++ bin/libs/interactive_menu.py
@@ -67,6 +67,56 @@ def _categoryPolynomial():
       SubCategory()
 
 
+_GlobalLfsr = Lfsr(16, RING_WITH_SPECIFIED_TAPS, [])
+
+def _categoryLfsrWithManualTaps_subDisplay():
+  global _GlobalLfsr
+  Text = f'Size: {_GlobalLfsr.getSize()}\nTAPS [\n'
+  for Tap in _GlobalLfsr.getTaps():
+    Text += f'  {Tap},\n'
+  Text += "]"
+  message_dialog(
+    title="Current Lfsr",
+    text=Text
+  ).run()
+  
+def _categoryLfsrWithManualTaps_subInfoDisplay():
+  global _GlobalLfsr
+  IsMax = "is MAXIMUM" if _GlobalLfsr.isMaximum() else "is NOT MAXIMUM"
+  P = Polynomial.decodeUsingBerlekampMassey(_GlobalLfsr)
+  PP = "PRIMITIVE" if P.isPrimitive() else "NOT PRIMITIVE"
+  Poly = f'Decoded polynomial: {P} ({PP})'
+  Text = IsMax + "\n" + Poly
+  message_dialog(
+    title="Information",
+    text=Text
+  ).run()
+  
+
+def _categoryLfsrWithManualTaps_subCreate():
+  global _GlobalLfsr
+  Size = MainMenu_static.getLfsrSize()
+  if Size is None:
+    return
+  Taps = MainMenu_static.getTaps()
+  if Taps is None:
+    return
+  del _GlobalLfsr
+  _GlobalLfsr = Lfsr(Size, RING_WITH_SPECIFIED_TAPS, Taps)
+  _categoryLfsrWithManualTaps_subDisplay()
+  
+def _categoryLfsrWithManualTaps_subMakeDual():
+  global _GlobalLfsr
+  _GlobalLfsr = _GlobalLfsr.getDual()
+  _categoryLfsrWithManualTaps_subDisplay()
+  
+
+def _categoryLfsrWithManualTaps():
+  SubCategory = -1
+  while SubCategory is not None:
+    SubCategory = MainMenu_static._lfsr_manual_taps_menu.run()
+    if SubCategory is not None:
+      SubCategory()
 
 
 
@@ -77,18 +127,28 @@ class MainMenu_static:
     title="Main menu",
     text="Choose category",
     values=[
-      (_categoryPolynomial, "Primitive polynomials"),
-      (2, "Maximum Rungs")
+      (_categoryPolynomial,                 "Primitive polynomials"),
+      (_categoryLfsrWithManualTaps,         "Ring generators with manually specified taps")
     ]
   )
   _prim_polys_menu = radiolist_dialog(
     title="Primitive polynomials",
     text="What you want to do:",
     values=[
-      (_categoryPolynomial_checkPrimitive, "Check if a given polynomial is primitive"),
-      (_categoryPolynomial_print, "Print primitives"),
-      (_categoryPolynomial_printDense, "Print dense primitives"),
-      (_categoryPolynomial_decode, "Decode using Berlekamp-Massey algorithm")
+      (_categoryPolynomial_checkPrimitive,  "Check if a given polynomial is primitive"),
+      (_categoryPolynomial_print,           "Print primitives"),
+      (_categoryPolynomial_printDense,      "Print dense primitives"),
+      (_categoryPolynomial_decode,          "Decode using Berlekamp-Massey algorithm")
+    ]
+  )
+  _lfsr_manual_taps_menu = radiolist_dialog(
+    title="Ring generators with manually specified taps",
+    text="What you want to do:",
+    values=[
+      (_categoryLfsrWithManualTaps_subDisplay,  "Display ring generator"),
+      (_categoryLfsrWithManualTaps_subCreate,   "Define ring generator"),
+      (_categoryLfsrWithManualTaps_subInfoDisplay, "is maximum? What is the polynomial?"),
+      (_categoryLfsrWithManualTaps_subMakeDual,   "Make dual ring")
     ]
   )
   _input_polynomial = input_dialog(
@@ -115,6 +175,28 @@ class MainMenu_static:
     title="Sequence?", 
     text="Enter a sequence of 1s and 0s:"
   )
+  _input_lfsr_size = input_dialog(
+    title="Lfsr size",
+    text="Enter size of the Lfsr:"
+  )
+  _input_taps = input_dialog(
+    title="Taps list",
+    text="""Enter taps. Example string:
+    [3,6], [6,2]
+    ..which means two taps: from FF3 output to the XOR at FF6 input and the second tap from FF6 output to the XOR at FF2 input."""
+  )
+  
+  def getTaps() -> list:
+    while 1:
+      Result = MainMenu_static._input_taps.run()
+      if Result is None:
+        return None
+      try:
+        R = list(ast.literal_eval(f'[{Result}]'))
+        return R
+      except:
+        continue
+    
   
   def getBinSequence() -> str:
     Result = MainMenu_static._input_bin_sequence.run()
@@ -147,14 +229,23 @@ class MainMenu_static:
         return 1
     
   def getN() -> int:
+    Result = MainMenu_static._input_n_results.run()
+    if Result is None:
+      return None
+    try:
+      return int(ast.literal_eval(Result))
+    except:
+      return 0
+      
+  def getLfsrSize() -> int:
     while 1:
-      Result = MainMenu_static._input_n_results.run()
+      Result = MainMenu_static._input_lfsr_size.run()
       if Result is None:
         return None
       try:
         return int(ast.literal_eval(Result))
       except:
-        return 0
+        continue
   
   def getPolynomial() -> Polynomial:
     while 1:
