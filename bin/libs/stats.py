1aef033603d959da0cc7f14958837148c9cee309
Maciej <maciej.trawka@icloud.com>
2024-03-15 19:51:30 +0100

    interpolate

--- bin/libs/stats.py
+++ bin/libs/stats.py
@@ -442,3 +442,28 @@ class Stats:
     for I in Data:
       Chi2 += ((I - Avg) / Avg)
     return Chi2
+  
+  def predictLinear(x : float, x1 : float, y1 : float, x2 : float, y2 : float) -> float:
+    a = (y1 - y2) / (x1 - x2)
+    b = y1 - a * x1
+    return x * a + b
+  
+  def predict(x : float, xPoints : list, yPoints : list) -> float:
+    Points = [(xp, yp) for xp, yp in zip(xPoints, yPoints)]
+    if len(Points) < 1:
+      return None
+    if len(Points) == 1:
+      return Points[0][1]
+    Points.sort(key = lambda x: x[0])
+    if len(Points) == 2:
+      return Stats.predictLinear(x, Points[0][0], Points[0][1], Points[1][0], Points[1][1])
+    if len(Points) == 3:
+      return Stats.predictQuadratic(x, Points[0][0], Points[0][1], Points[1][0], Points[1][1], Points[2][0], Points[2][1])
+    if x <= Points[1][0]:
+      return Stats.predictLinear(x, Points[0][0], Points[0][1], Points[1][0], Points[1][1])
+    if x >= Points[-1][0]:
+      return Stats.predictLinear(x, Points[-2][0], Points[-2][1], Points[-1][0], Points[-1][1])
+    for i in range(len(Points)-1):
+      if Points[i][0] <= x <= Points[i+1][0]:
+        return Stats.predictLinear(x, Points[i][0], Points[i][1], Points[i+1][0], Points[i+1][1])
+    return None
