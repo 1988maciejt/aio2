209420c1cecd4b326d36fc9cad2dbee9026975c9
Trawka <maciej.trawka@siemens.com>
2025-01-03 12:58:01 +0100

    compressability

--- bin/libs/research_projects/testkompress_advisor/testkompress_advisor.py
+++ bin/libs/research_projects/testkompress_advisor/testkompress_advisor.py
@@ -3,12 +3,136 @@ from bitarray import *
 import bitarray.util as bau
 from random import *
 from copy import deepcopy
+from math import ceil
 
 _TEST_CUBE_BITARRAYS = 0
 _TEST_CUBE_IMPLEMENTATION_LOCKED = 0
 
+
+class TestDataDecompressor:
+    pass
 class TestCube:
     pass
+class TestCubeSet:
+    pass
+class EdtStructure:
+    pass
+
+class TestDataDecompressor:
+    
+    __slots__ = ('InputCount', 'OutputCount', 'LfsrLength', 'ScanLength')
+    
+    def __init__(self, InputCount : int, OutputCount : int, LfsrLength : int, ScanLength :  int):
+        self.InputCount = InputCount
+        self.OutputCount = OutputCount
+        self.LfsrLength = LfsrLength
+        if type(ScanLength) is int:
+            self.ScanLength = ScanLength
+        elif type(ScanLength) is TestCube:
+            self.ScanLength = int(ceil(len(ScanLength) / OutputCount))
+            
+    def __repr__(self) -> str:
+        return f"TestDataDecompressor({self.InputCount}, {self.OutputCount}, {self.LfsrLength}, {self.ScanLength})"
+        
+    def __str__(self) -> str:
+        return f"InputCount={self.InputCount}, OutputCount={self.OutputCount}, LfsrLength={self.LfsrLength}, ScanLength={self.ScanLength}"
+        
+    def getSpecifiedBitPerCycleDict(self, Cube : TestCube) -> dict:
+        Result = {}
+        TCPos = Cube.getSpecifiedBitPositions()
+        for Pos in TCPos:
+            PosInScan = Pos % self.ScanLength
+            Result[PosInScan] = Result.get(PosInScan, 0) + 1
+        return Result
+    
+    def getMaximumSpecifiedBitPerCycleToBeCompressable(self) -> int:
+        return int(self.LfsrLength + ((self.ScanLength - 1) * self.InputCount))
+    
+    def getMaximumCubeBitsPerDecompressor(self) -> int:
+        return int(self.OutputCount * self.ScanLength)
+    
+    def isCompressable(self, Cube, MinBatteryCharge : float = 0.1) -> bool:
+        SpecifiedBitsPerCycle = self.getSpecifiedBitPerCycleDict(Cube)
+        if Cube.getSpecifiedCount() > self.getMaximumSpecifiedBitPerCycleToBeCompressable():
+            return False
+        # Battery model
+        MinChrg = int(ceil(MinBatteryCharge * self.LfsrLength))
+        BatteryChrg = self.LfsrLength
+        for i in range(self.ScanLength):
+            BatteryChrg -= SpecifiedBitsPerCycle.get(i, 0)
+            if BatteryChrg < MinChrg:
+                return False
+            BatteryChrg += self.InputCount
+        return True
+
+
+class EdtStructure:
+    
+    __slots__ = ('_decompressors', '_scan_len')
+    
+    def __init__(self, Decompressors : list = None):
+        self._decompressors = []
+        self._scan_len = 0
+        if type(Decompressors) is list:
+            for Decompressor in Decompressors:
+                self.addDecompressor(Decompressor)
+        if type(Decompressors) is TestDataDecompressor:
+            self.addDecompressor(Decompressors)
+                
+    def __len__(self) -> int:
+        return len(self._decompressors)
+    
+    def __repr__(self) -> str:
+        return f"EdtStructure({self._decompressors})"
+    
+    def __str__(self):
+        Result = "["
+        for Decompressor in self._decompressors:
+            Result += str(Decompressor) + "\n"
+        Result += "]"
+        return Result
+                
+    def getScanLength(self) -> int:
+        return self._scan_len
+    
+    def getDecompressorCount(self) -> int:
+        return len(self._decompressors)
+    
+    def getInputCount(self) -> int:
+        return sum([Decompressor.InputCount for Decompressor in self._decompressors])
+    
+    def getScanChainCount(self) -> int:
+        return sum([Decompressor.OutputCount for Decompressor in self._decompressors])
+    
+    def getFFCount(self) -> int:
+        return sum([Decompressor.LfsrLength for Decompressor in self._decompressors])
+    
+    def getMaximumSpecifiedBitPerCycleToBeCompressable(self) -> int:
+        return max([Decompressor.getMaximumSpecifiedBitPerCycleToBeCompressable() for Decompressor in self._decompressors])
+    
+    def getCubeBitsPerDecompressor(self) -> list:
+        return [Decompressor.getMaximumCubeBitsPerDecompressor() for Decompressor in self._decompressors]
+        
+    def addDecompressor(self, Decompressor : TestDataDecompressor) -> bool:
+        if self._scan_len == 0:
+            self._scan_len = Decompressor.ScanLength
+        elif Decompressor.ScanLength != self._scan_len:
+            Aio.printError(f"Decompressor ScanLength must be {self._scan_len}.")
+            return False
+        self._decompressors.append(Decompressor)
+        return True
+    
+    def isCompressable(self, Cube : TestCube, MinBatteryCharge : float = 0.1) -> bool:
+        if Cube.getSpecifiedCount() > self.getMaximumSpecifiedBitPerCycleToBeCompressable():
+            return False
+        SubCubes = Cube.splitIntoSubCubes(self.getCubeBitsPerDecompressor())
+        for i in range(len(self._decompressors)):
+            if not self._decompressors[i].isCompressable(SubCubes[i], MinBatteryCharge):
+                return False
+        return True
+        
+    
+
 class TestCube:
     
     __slots__ = ('_specified_bit_positions', '_specified_bit_values', "Age")
@@ -40,7 +164,7 @@ class TestCube:
                     SDict[i] = 1 if random() < P1 else 0
             return TestCube(SDict, Length)
     
-    def __init__(self, SpecifiedBits : str = '', LenIfSpecifiedSDict : int = 0):
+    def __init__(self, SpecifiedBits : str = '', LenOfTestCubeIfDictImplementation : int = 0):
         global _TEST_CUBE_BITARRAYS, _TEST_CUBE_IMPLEMENTATION_LOCKED
         _TEST_CUBE_IMPLEMENTATION_LOCKED = 1
         if _TEST_CUBE_BITARRAYS:
@@ -48,15 +172,18 @@ class TestCube:
             self._specified_bit_values = bitarray()
         else:
             self._specified_bit_positions = {}
-            self._specified_bit_values = 0
+            self._specified_bit_values = LenOfTestCubeIfDictImplementation
         self.Age = 0
         if type(SpecifiedBits) is str:
             self.setBits(SpecifiedBits)
         elif type(SpecifiedBits) is dict:
-            self.setBits(SpecifiedBits, LenIfSpecifiedSDict)
+            self.setBits(SpecifiedBits, LenOfTestCubeIfDictImplementation)
         elif type(SpecifiedBits) is int:
-            self._specified_bit_positions = bau.zeros(SpecifiedBits)
-            self._specified_bit_values = bau.zeros(SpecifiedBits)
+            if _TEST_CUBE_BITARRAYS:
+                self._specified_bit_positions = bau.zeros(SpecifiedBits)
+                self._specified_bit_values = bau.zeros(SpecifiedBits)
+            else:
+                self._specified_bit_values = SpecifiedBits
         elif type(SpecifiedBits) is TestCube:
             self._specified_bit_positions = deepcopy(SpecifiedBits._specified_bit_positions)
             self._specified_bit_values = deepcopy(SpecifiedBits._specified_bit_values)
@@ -84,14 +211,45 @@ class TestCube:
     def __repr__(self):
         return f"TestCube(\"{str(self)}\")"
     
-    def setBits(self, SpecifiedBits : str, LenIfDict : int = 0):
+    def __getitem__(self, index):
+        global _TEST_CUBE_BITARRAYS
+        if isinstance(index, slice):
+            start, stop, step = index.start, index.stop, index.step
+            if step is not None:
+                Aio.printError("Step is not supported.")
+                return None
+            if _TEST_CUBE_BITARRAYS:
+                Result = TestCube()
+                Result._specified_bit_positions = self._specified_bit_positions[start:stop]
+                Result._specified_bit_values = self._specified_bit_values[start:stop]
+                return Result
+            else:
+                Result = TestCube(stop - start - 1)
+                Result._specified_bit_positions = {i-start: self._specified_bit_positions[i] for i in range(start, stop) if i in self._specified_bit_positions}
+                return Result
+        else:
+            return self.getBit(index)
+    
+    def __setitem__(self, index, value):
+        self.setBit(index, value)
+    
+    def splitIntoSubCubes(self, SubCubesLen : list) -> list:
+        Result = []
+        Start = 0
+        for SCLen in SubCubesLen:
+            Stop = Start + SCLen
+            Result.append(self[Start:Stop])
+            Start = Stop
+        return Result
+    
+    def setBits(self, SpecifiedBits : str, TestCubeLenIfDictImplementation : int = 0):
         global _TEST_CUBE_BITARRAYS
         # 0, 1 - values
         # X - don't care
         if _TEST_CUBE_BITARRAYS:
             if type(SpecifiedBits) is dict:
-                self._specified_bit_positions = bau.zeros(LenIfDict)
-                self._specified_bit_values = bau.zeros(LenIfDict)
+                self._specified_bit_positions = bau.zeros(TestCubeLenIfDictImplementation)
+                self._specified_bit_values = bau.zeros(TestCubeLenIfDictImplementation)
                 for item in SpecifiedBits.items():
                     self._specified_bit_positions[item[0]] = 1
                     self._specified_bit_values[item[0]] = item[1]
@@ -105,7 +263,7 @@ class TestCube:
         else:
             if type(SpecifiedBits) is dict:
                 self._specified_bit_positions = SpecifiedBits
-                self._specified_bit_values = LenIfDict
+                self._specified_bit_values = TestCubeLenIfDictImplementation
             else:
                 SpecifiedBits = SpecifiedBits.upper()
                 for i in range(len(SpecifiedBits)):
@@ -114,6 +272,20 @@ class TestCube:
                     elif SpecifiedBits[i] == '1':
                         self._specified_bit_positions[i] = 1
                 self._specified_bit_values = len(SpecifiedBits)
+                
+    def getFillRate(self) -> float:
+        global _TEST_CUBE_BITARRAYS
+        if _TEST_CUBE_BITARRAYS:
+            return self._specified_bit_positions.count(1) / len(self)
+        else:
+            return len(self._specified_bit_positions) / len(self)
+        
+    def getSpecifiedBitPositions(self) -> list:
+        global _TEST_CUBE_BITARRAYS
+        if _TEST_CUBE_BITARRAYS:
+            return [i for i in self._specified_bit_positions.search(1)]
+        else:
+            return list(self._specified_bit_positions.keys())
         
     def copy(self) -> TestCube:
         Result = TestCube(self)
@@ -199,8 +371,6 @@ class TestCube:
             
     
 class TestCubeSet:
-    pass
-class TestCubeSet:
     
     __slots__ = ('_cubes')
     
@@ -218,6 +388,15 @@ class TestCubeSet:
     
     def __repr__(self):
         return "TestCubeSet()"
+    
+    def __getitem__(self, index) -> TestCube:
+        if isinstance(index, slice):
+            start, stop, step = index.start, index.stop, index.step
+            Result = TestCubeSet()
+            Result._cubes = self._cubes[start:stop:step]
+            return Result
+        else:
+            return self._cubes[index]
     
     def addCube(self, Cube : TestCube):
         self._cubes.append(Cube)
@@ -253,52 +432,78 @@ class TestCubeSet:
             return False
     
     def autoMerge(self, PreSort : bool = True):
-        if PreSort:
-            self.sort()
-        DidSomething = True
-        while DidSomething:
-            DidSomething = False
-            for i in range(len(self._cubes)):
-                if self._cubes[i].Age < 0:
-                    continue
-                ThisCube = self._cubes[i].copy()
-                for j in range(i + 1, len(self._cubes)):
-                    if self._cubes[j].Age < 0:
-                        continue
-                    ThisCubeAux = ThisCube.copy()
-                    if ThisCubeAux.mergeWithAnother(self._cubes[j]):
-                        # Check if ThisCubeAux is OK
-                        if 1: # if OK
-                            ThisCube = ThisCubeAux
-                            ThisCube.Age = -2       # Mark as merged
-                            self._cubes[j].Age = -1 # Mark as used
-                            DidSomething = True
-                        else:
-                            self._cubes[j].Age += 1 # Age++
-                    else:
-                        self._cubes[j].Age += 1     # Age++
-                self._cubes[i] = ThisCube
-            self.sort()
-
-    def splitMergedAndNotmerged(self) -> tuple:
-        """Returns two TestCubeSet objects: (Merged_cubes, NotMerged_cubes). Cubes used for merging are ignored."""
-        Merged = TestCubeSet()
-        NotMerged = TestCubeSet()
-        for Cube in self._cubes:
-            if Cube.Age == -2:
-                Merged.addCube(Cube)
-            elif Cube.Age >= 0:
-                NotMerged.addCube(Cube)
-        return Merged, NotMerged
+        pass
     
-    def splitIntoBufferAndRest(self, BufferLength : int = 100) -> tuple:
-        """Returns two TestCubeSet objects: (Buffer, Rest)."""
+    def getCube(self, index : int) -> TestCube:
+        return self._cubes[index]
+    
+    def getSubCubeSets(self, SubSetSize : int) -> list:
+        Result = []
+        for i in range(0, len(self), SubSetSize):
+            TestCubeSubset = TestCubeSet()
+            TestCubeSubset._cubes = self._cubes[i:i+SubSetSize]
+            Result.append(TestCubeSubset)
+        return Result
+    
+    def _mergingRound(self, Edt : EdtStructure, PatternCount : int = 64, MinBatteryCharge : float = 0.1, Verbose : bool = False) -> tuple:
+        """Returns two TestCubeSet objects: (Patterns, Cubes)."""
+        Cubes = self.copy()
+        Patterns = TestCubeSet()
+        MaximumBitCount = Edt.getMaximumSpecifiedBitPerCycleToBeCompressable()
+        while len(Cubes) > 0 and len(Patterns) < PatternCount:
+            Cubes.sort()
+            Cube = Cubes.getCube(0)
+            ToBeRemovedFromBuffer = [0]
+            timer0 = time.time()
+            for i in range(1, len(Cubes)):
+                CubeAux = Cube.copy()
+                MergingResult = CubeAux.mergeWithAnother(Cubes._cubes[i])
+                if MergingResult and Edt.isCompressable(CubeAux, MinBatteryCharge):
+                    Cube = CubeAux
+                    ToBeRemovedFromBuffer.append(i)
+                    if Cube.getSpecifiedCount()-1 >= MaximumBitCount:
+                        break
+                else:
+                    Cubes._cubes[i].Age += 1
+            timer0 = time.time()
+            Patterns._cubes.append(Cube)
+            for i in reversed(ToBeRemovedFromBuffer):
+                del Cubes._cubes[i]
+        return Patterns, Cubes
+    
+    def merge(self, Edt : EdtStructure, BufferLength : int = 512, PatternCountPerRound : int = 64, MinBatteryCharge : float = 0.1, Verbose : bool = False) -> TestCubeSet:
+        self.resetAge()
         Buffer = TestCubeSet()
-        Rest = TestCubeSet()
-        Buffer._cubes = self._cubes[:BufferLength]
-        Rest._cubes = self._cubes[BufferLength:]
-        return Buffer, Rest
-
+        Buffer._cubes = self._cubes[0:BufferLength]
+        index = BufferLength
+        Patterns, Buffer = Buffer._mergingRound(Edt, PatternCountPerRound, MinBatteryCharge, Verbose)
+        if Verbose:
+            print(f"Furst round finished. BufferLen={len(Buffer)}, PatternsLen={len(Patterns)}")
+        while index < len(self):
+            HowManyToAdd = BufferLength - len(Buffer)
+            Buffer._cubes += self._cubes[index:index+HowManyToAdd]
+            index += HowManyToAdd
+            SubPatterns, Buffer = Buffer._mergingRound(Edt, PatternCountPerRound, MinBatteryCharge, Verbose)
+            Patterns._cubes += SubPatterns._cubes
+            if Verbose:
+                print(f"Next round finished. BufferLen={len(Buffer)}, PatternsLen={len(Patterns)}")
+        if len(Buffer) > 0:
+            SubPatterns, Buffer = Buffer._mergingRound(Edt, len(Buffer), MinBatteryCharge, Verbose)
+            Patterns._cubes += SubPatterns._cubes
+            if Verbose:
+                print(f"Last. BufferLen={len(Buffer)}, PatternsLen={len(Patterns)}")
+        print(f"FINISHED BufferLen={len(Buffer)}, PatternsLen={len(Patterns)}")
+        return Patterns
+    
+    def removeNotCompressable(self, Edt : EdtStructure) -> int:
+        ToBeRemoved = []
+        for i in range(len(self)):
+            if not Edt.isCompressable(self.getCube(i)):
+                ToBeRemoved.append(i)
+        for i in reversed(ToBeRemoved):
+            self.removeCube(i)
+        return len(ToBeRemoved)
+    
 
 class TestCubeMerger:
     pass
